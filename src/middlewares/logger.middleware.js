bde33a93411c91a709b453658560f0cc1c8b3a23
Mrutunjaya Panda <mrutunjayapanda28@gmail.com>
2026-04-03 02:35:29 +0530

    used inbuilt winston package by npm for more effective & standardized logging of informations for debugging & monitoring purposes

--- src/middlewares/logger.middleware.js
+++ src/middlewares/logger.middleware.js
@@ -4,36 +4,42 @@
 //Also herein we will see how to write in files using another method of nodeJS to create & write in log files.
 
 import fs from "fs";
-//we are going to use promise based method of fs module to write in log files, so we will use the fs.promises API of fs module, which provides promise based methods for file operations.
+import winston from "winston";
 
-//this promises object will have methods like writeFile, appendFile, readFile, etc. which we can use to perform file operations in an asynchronous way using promises.
-//yes we could have used fs.writeFile and fs.appendFile methods which are callback based, but using promises is more cleaner and easier to read and maintain, and it also helps to avoid callback hell,
-//so we will use fs.promises API of fs module for file operations in this logger middleware.
-const fsPromise = fs.promises;
+const logger = winston.createLogger({
+    level: 'info',//here level represents the level of log messages that will be logged in the log file, we can set it to different levels like error, warn, info, verbose, debug, silly, etc. but for now we will set it to info level to log all the log messages with info level and above (error and warn) in the log file.
+    //format: winston.format.json(),//here format represents the format in which the log data will be stored in the log file, we can also use other formats like simple, prettyPrint, etc. but for now we will use json format to store the log data in the log file in a structured format which can be easily parsed and analyzed later.
+    format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+    defaultMeta: { service: 'user-service' },//here defaultMeta represents the default metadata that will be added to each log message, we can add any metadata that we want to add to each log message, but for now we will add a default metadata with the service name as user-service to identify the source of the log messages in case we have multiple services logging in the same log file.
+    transports: [
+        //new winston.transports.File({ filename: 'log.txt' })//normal text file.
+        new winston.transports.File({ filename: 'combined.log' })//here we are creating a new transport which will write the log data in the combined.log file, we can also create multiple transports to write the log data in different files or to send the log data to different destinations like console, database, etc. but for now we will create only one transport to write the log data in the combined.log file.
+    ]
+});
 
-async function log(logData) {
-  try {
-    logData = new Date().toString() + ". Log Data: " + logData + "\n";
-    // await fsPromise.writeFile('log.txt', logData);//this will overwrite the existing log data in the log.txt file with the new log data, so we can use appendFile method instead of writeFile method to append the new log data to the existing log data in the log.txt file.
-    await fsPromise.appendFile("log.txt", logData);
-  } catch (err) {
-    console.log(err);
-  }
-}
 
 const loggerMiddleware = async (req, res, next) => {
-  if (!req.url.includes("signIn") && !req.url.includes("signUp")) {
-    //1. log request body
-    await log("Request Body: " + JSON.stringify(req.body));
-    //await log('Request Body: ' + req.body); //this will log the request body in the log.txt file, but it will not be in a readable format, so we can use JSON.stringify method to convert the request body object into a string format before logging it in the log.txt file, so that it will be in a readable format in the log.txt file.
-    //2. log request method and url
-    await log("Request Method: " + req.method + ", Request URL: " + req.url); //because url is already a string, so we can directly log it without using JSON.stringify method, but for request body we need to use JSON.stringify method to convert the request body object into a string format before logging it in the log.txt file, so that it will be in a readable format in the log.txt file.
-    // //3. log time of request
-    // await log('Time of Request: ' + new Date().toString());
+  if (!req.url.includes("signin") && !req.url.includes("signup")) {
+    // //1. log request body
+    //  logger.info("Request Body: " + JSON.stringify(req.body));
+    // //await log('Request Body: ' + req.body); //this will log the request body in the log.txt file, but it will not be in a readable format, so we can use JSON.stringify method to convert the request body object into a string format before logging it in the log.txt file, so that it will be in a readable format in the log.txt file.
+    // //2. log request method and url
+    //  logger.info("Request Method: " + req.method + ", Request URL: " + req.url); //because url is already a string, so we can directly log it without using JSON.stringify method, but for request body we need to use JSON.stringify method to convert the request body object into a string format before logging it in the log.txt file, so that it will be in a readable format in the log.txt file.
+    // // //3. log time of request
+    // // await log('Time of Request: ' + new Date().toString());
+    logger.info({
+        url: req.originalUrl,
+        method: req.method,
+        body: req.body,
+        timestamp: new Date().toISOString()
+    });
     next();
   } else {
     //for signIn and signUp APIs, we will not log the request body as it may contain sensitive information like password, so we will only log the request method and url for these APIs.
-    await log("Request Method: " + req.method + ", Request URL: " + req.url);
+     logger.info("Request Method: " + req.method + ", Request URL: " + req.url);
     next();
   }
 };
