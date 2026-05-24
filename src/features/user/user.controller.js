19da6989f6f79c2d50d57ff14fb2f169c49e392b
Mrutunjaya Panda <mrutunjayapanda28@gmail.com>
2026-05-25 00:08:56 +0530

    implemented repository folder for database operations for better separate of concern & bycrypt module for password hashing for security reasons.

--- src/features/user/user.controller.js
+++ src/features/user/user.controller.js
@@ -1,12 +1,26 @@
 import UserModel from "./user.model.js";
 import jwt from "jsonwebtoken";
+import UserRepository from "./user.repository.js";
+//for hashing password
+import bcrypt from "bcrypt";
+import { logger } from "../../middlewares/logger.middleware.js";
 export default class UserController{
-    //since it returns a promise, we need to use async/await to handle the promise, or we can also use then and catch to handle the promise, but for now we will use async/await to handle the promise.
-    //embed this in a try/catch block as any async operation can throw an error, and we need to handle that error properly to avoid crashing the server and to send proper error response to the client.
+    constructor(){
+        this.userRepository = new UserRepository();
+    }
     async signUp(req, res){
         try{
             const {name, email, password, type} = req.body;
-            const newUser = await UserModel.SignUp(name, email, password, type);
+            //const newUser = await UserModel.SignUp(name, email, password, type);
+
+            //hash the password before storing it in the database, so that even if someone gets access to the database, they will not be able to see the actual password of the user, instead they will see the hashed password, which is not useful for them, because hashing is a one way function, which means that we cannot get the original password from the hashed password, 
+            // so even if someone gets access to the database, they will not be able to see the actual password of the user, and they will not be able to use the hashed password to login to the application, 
+            // because when user tries to login to the application, we will hash the password entered by the user and compare it with the hashed password stored in the database, if both are same then only we will allow the user to login to the application, 
+            // otherwise we will reject the login attempt of the user.
+            const hashedPassword = await bcrypt.hash(password, 10); //10 is the number of salt rounds, which means that the hashing algorithm will run 10 times to generate the hashed password, which makes it more secure, because it will take more time to generate the hashed password, and it will also make it more difficult for attackers to crack the hashed password using brute force attack, because they will have to run the hashing algorithm multiple times to generate the hashed password, which will take more time and resources for them, so it will make it more difficult for attackers to crack the hashed password using brute force attack.
+
+            const newUser = new UserModel(name, email, hashedPassword, type);
+            await this.userRepository.SignUp(newUser);
             res.status(201).send(newUser);
         } catch(err){
             res.status(500).send("Error occurred while signing up the user. Please try again later.");
@@ -14,19 +28,52 @@ export default class UserController{
     }
 
     //after successful login, we will generate a JWT token and send it to the client, which will be used for authentication and authorization for protected routes.
-    signIn(req, res){
-        const {email, password} = req.body;
-        const user = UserModel.SignIn(email, password);
-        if(!user){
-            res.status(401).send("Incorrect credentials");
-        } else {
-            //1. create token for that user
-            //secret key should be stored in environment variable, but for simplicity we are hardcoding it here, but in real world application we should never hardcode the secret key in the code, we should always store it in environment variable and access it from there.
-            const token = jwt.sign({id: user.id, email: user.email}, "g4NaMBkTIcNEq9HhPbNy5QfdRZ7hYmfmja5E9GOk5bc=",{
-                expiresIn: "1h"
-            });
-            //2. send token to client
-            res.status(200).send(token);
+    async signIn(req, res, next){
+        try{
+            //1. find user by email.
+            const user = await this.userRepository.findByemail(req.body.email);
+            if(!user){
+                res.status(401).send("Incorrect credentials");
+            }else{
+                //2. use compare method of bcrypt to compare the password entered by the user with the hashed password stored in the database, if both are same then only we will allow the user to login to the application, otherwise we will reject the login attempt of the user.
+                const isPasswordmatch = await bcrypt.compare(req.body.password, user.password);
+                if(!isPasswordmatch){
+                    res.status(401).send("Incorrect credentials");
+                }else{
+                    //1. create token for that user
+                    //secret key should be stored in environment variable, but for simplicity we are hardcoding it here, but in real world application we should never hardcode the secret key in the code, we should always store it in environment variable and access it from there.
+                    const token = jwt.sign({id: user._id, email: user.email}, "g4NaMBkTIcNEq9HhPbNy5QfdRZ7hYmfmja5E9GOk5bc=",{
+                    expiresIn: "1h"
+                   });
+                   //console.log("user details:", user);
+                   //2. send token to client
+                   res.status(200).send(token);
+                }
+              }
         }
+
+        //     const {email, password} = req.body;
+        //     const user = await this.userRepository.SignIn(email, password);
+        //   if(!user){
+        //     res.status(401).send("Incorrect credentials");
+        //   } else {
+        //     //1. create token for that user
+        //     //secret key should be stored in environment variable, but for simplicity we are hardcoding it here, but in real world application we should never hardcode the secret key in the code, we should always store it in environment variable and access it from there.
+        //     const token = jwt.sign({id: user.id, email: user.email}, "g4NaMBkTIcNEq9HhPbNy5QfdRZ7hYmfmja5E9GOk5bc=",{
+        //         expiresIn: "1h"
+        //     });
+        //     //2. send token to client
+        //     res.status(200).send(token);
+        //   }
+        // }
+        catch(err){
+            //we can also log our error in combined.log by:-
+            logger.error("Error occurred while signing in the user: " + err.message);
+
+            console.error("Error occurred while signing in the user:", err);
+            //throw new Error("Error occurred while signing in the user. Please try again later.");
+            next(err);//this will pass the error to the application level error handling middleware defined in our server.js file, which will catch the error and send a proper response to the client based on the type of error, let's see how to do that.
+        }
+        
     }
 }
--- src/features/user/user.routes.js
+++ src/features/user/user.routes.js
@@ -6,7 +6,16 @@ const userRouter = express.Router();
 
 const userController = new UserController();
 
-userRouter.post("/signup", userController.signUp);
-userRouter.post("/signin", userController.signIn);
+//userRouter.post("/signup", userController.signUp);
+//we have to change this below because we are no more using static method for signUp in user model, instead we are using instance method of user controller to handle the sign up logic, 
+// so we have to change the route handler accordingly to call the signUp method of user controller instance, let's see how to do that.
+userRouter.post("/signup", (req,res) => {
+    userController.signUp(req,res);
+});
+// userRouter.post("/signin", userController.signIn);
+//similarly we will do for signIn route.
+userRouter.post("/signin", (req,res, next) => {
+    userController.signIn(req,res, next);
+});
 
 export default userRouter;
